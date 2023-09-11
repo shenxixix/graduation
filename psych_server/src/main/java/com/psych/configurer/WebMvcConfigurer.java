efdfdfe02179a246e596bccd52dda7a9c81c94e8
shenxi <shenxi@carsjoy.com>
2023-09-11 19:46:47 +0800

    提交

--- psych_server/src/main/java/com/psych/configurer/WebMvcConfigurer.java
+++ psych_server/src/main/java/com/psych/configurer/WebMvcConfigurer.java
@@ -60,14 +60,18 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
             public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) {
                 Result result = new Result();
                 if (e instanceof ServiceException) {//业务失败的异常，如“账号或密码错误”
-                    result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
+                    result.setCode(ResultCode.FAIL.code() + "");
+                    result.setMessage(e.getMessage());
                     logger.info(e.getMessage());
                 } else if (e instanceof NoHandlerFoundException) {
-                    result.setCode(ResultCode.NOT_FOUND).setMessage("接口 [" + request.getRequestURI() + "] 不存在");
+                    result.setCode(ResultCode.NOT_FOUND.code() + "");
+                    result.setMessage("接口 [" + request.getRequestURI() + "] 不存在");
                 } else if (e instanceof ServletException) {
-                    result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
+                    result.setCode(ResultCode.FAIL + "");
+                    result.setMessage(e.getMessage());
                 } else {
-                    result.setCode(ResultCode.INTERNAL_SERVER_ERROR).setMessage("接口 [" + request.getRequestURI() + "] 内部错误，请联系管理员");
+                    result.setCode(ResultCode.INTERNAL_SERVER_ERROR + "");
+                    result.setMessage("接口 [" + request.getRequestURI() + "] 内部错误，请联系管理员");
                     String message;
                     if (handler instanceof HandlerMethod) {
                         HandlerMethod handlerMethod = (HandlerMethod) handler;
